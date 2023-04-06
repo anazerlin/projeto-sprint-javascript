0b35111216ac705a3cc74b708c82fadf92efae9e
ana <analaura.chipweb@gmail.com>
2023-04-06 10:25:47 -0300

    adicionando o js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//VALIDAÇÃO DE FORMULÁRIO
+const form = document.getElementById("myForm");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // impedir que a página recarregue ao enviar o formulário
+
+  if (name.value === "") {
+    alert("Por favor, preencha o campo de nome.");
+    return;
+  }
+
+  if (email.value === "") {
+    alert("Por favor, preencha o campo de e-mail.");
+    return;
+  }
+
+  if (password.value === "") {
+    alert("Por favor, preencha o campo de senha.");
+    return;
+  }
+
+  // Validar comprimento da senha
+  if (password.value.length < 6) {
+    alert("A senha deve ter pelo menos 6 caracteres.");
+    return;
+  }
+
+  // Se tudo estiver válido, enviar formulário
+  form.submit();
+});
+
+
+//MANIPULANDO EVENTO DE PÁGINA
+const button = document.getElementById("myButton");
+const paragraph = document.getElementById("myParagraph");
+
+// Adicionar evento de clique ao botão
+button.addEventListener("click", () => {
+  // Alterar o conteúdo do parágrafo
+  paragraph.textContent = "O botão foi clicado!";
+  
+  // Alterar a cor de fundo do parágrafo
+  paragraph.style.backgroundColor = "yellow";
+});
+
+// Adicionar evento de mouseover ao parágrafo
+paragraph.addEventListener("mouseover", () => {
+  // Alterar a cor de fundo do parágrafo
+  paragraph.style.backgroundColor = "lightblue";
+});
+
